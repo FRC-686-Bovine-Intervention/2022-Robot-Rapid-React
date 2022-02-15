a0e126a2b7fbbb8bc9b28d462cc32ea3f3433985
Noah Kang <noahkang82@gmail.com>
2022-02-15 16:56:49 -0500

    2/15/2022 Intake Code
    Intake Code - Shanmukha, Noah, Caleb

--- Robot 2022 Rapid React/src/main/java/frc/robot/Subsystems/Subsystems/Intake.java
+++ Robot 2022 Rapid React/src/main/java/frc/robot/Subsystems/Subsystems/Intake.java
@@ -1,12 +1,16 @@
 package frc.robot.Subsystems.Subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.robot.Subsystems.Subsystem;
+import frc.robot.Subsystems.Subsystems.Climber.ClimberState;
+
 
 /**<h4>Contains all code for the Intake subsystem</h4>*/
 public class Intake extends Subsystem {
@@ -47,10 +51,68 @@ public class Intake extends Subsystem {
         CLIMBING,
         CALIBRATING
     }
+    public enum outtakeState{
+        SCORING,
+        HIDING
+    }
+
     public IntakeState intakeStatus;
+    public outtakeState outtakeStatus;
 
     @Override
-    public void run(){}
+    public void run(){
+        if (!calibrated){
+            changeState(IntakeState.CALIBRATING);
+        }
+        switch(intakeStatus){
+            case DEFENSE:
+                setPos(ArmPosEnum.LOWERED);
+                RollerMotor.set(VictorSPXControlMode.PercentOutput, 0);
+                break;
+            case INTAKE:
+                setPos(ArmPosEnum.LOWERED);
+                RollerMotor.set(VictorSPXControlMode.PercentOutput, 0.75);
+                break;
+            case OUTTAKE:
+                switch (outtakeStatus){
+                    case SCORING:
+                        setPos(ArmPosEnum.RAISED);
+                        RollerMotor.set(VictorSPXControlMode.PercentOutput, -0.75);
+                        break;
+                    case HIDING:
+                        setPos(ArmPosEnum.LOWERED);
+                        RollerMotor.set(VictorSPXControlMode.PercentOutput, -0.3);
+                        break;
+                }
+                break;
+            case CLIMBING:
+                //Check if robot is in defense state
+                if (Climber.getInstance().ClimberStatusHistory.get(Climber.getInstance().ClimberStatusHistory.size()-1) != ClimberState.DEFENSE) break;
+
+                //Release climber and extend
+                Climber.getInstance().changeState(ClimberState.EXTEND);
+
+                //Set ArmPosEnum.RAISED
+                setPos(ArmPosEnum.RAISED);
+
+                //If power level is high, set it back to lower state
+                if (ArmMotor.getStatorCurrent() > 5) {setPos(ArmPosEnum.LOWERED);}
+
+                //Retract climber
+                Climber.getInstance().changeState(ClimberState.RETRACT);
+                break;
+            case CALIBRATING:
+                ArmMotor.set(TalonFXControlMode.PercentOutput, 0.2);
+                if (ArmMotor.getStatorCurrent() > 5)
+                {
+                    ArmMotor.set(TalonFXControlMode.PercentOutput, 0);
+                    changeState(IntakeState.DEFENSE);
+                    calibrated = true;
+                }
+                break;
+
+        }
+    }
 
     @Override
     public void runTestMode(){}
