73a1e2c01a06b6d36e7810c8a6772e27605c32da
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2022-02-16 17:07:37 -0500

    2/16/2022 Updated
    Worked on Climber.java
    and made small changes in Intake.java

--- Robot 2022 Rapid React/src/main/java/frc/robot/Subsystems/Subsystems/Climber.java
+++ Robot 2022 Rapid React/src/main/java/frc/robot/Subsystems/Subsystems/Climber.java
@@ -30,34 +30,76 @@ public class Climber extends Subsystem {
 
     public enum ClimberState {
         DEFENSE,
-        EXTEND,
-        RETRACT,
+        LEAN_FORWARD,
+        LEAN_BACKWARD,
         CALIBRATING
-    }
+    } 
     public ArrayList<ClimberState> ClimberStatusHistory = new ArrayList<>();
+
+    public enum ClimberArmPos {
+        EXTEND,
+        RETRACT
+    }
+    private ClimberArmPos currentArmPos;
+
+    public ClimberArmPos getCurrentArmPos() {
+        return currentArmPos;
+    }
+
+    public void changeCurrentArmPos(ClimberArmPos currentArmPos) {
+        this.currentArmPos = currentArmPos;
+    }
+
+    public boolean isArmAtPos(ClimberArmPos state) {
+        return currentArmPos == state;
+    }
 
     @Override
     public void run(){
+        if (!calibrated){
+            changeState(ClimberState.CALIBRATING);
+        }
+
         switch (getClimberStatus()){
             case DEFENSE:
-                if (!isAtPos(ClimberPos.RETRACT)){
-                    changeState(ClimberPos.RETRACT);
-                }
-                break;
+                if (!isAtPos(ClimberState.LEAN_BACKWARD)){ changeState(ClimberState.LEAN_BACKWARD); }
+            break;
+            case LEAN_FORWARD: changeCurrentArmPos(ClimberArmPos.EXTEND); break;
+            case LEAN_BACKWARD: changeCurrentArmPos(ClimberArmPos.RETRACT); break;
+            case CALIBRATING:
+                changeState(ClimberState.LEAN_BACKWARD);
+                changeCurrentArmPos(ClimberArmPos.RETRACT);
+            break;
+        }
+
+        switch (currentArmPos) {
             case EXTEND:
-                if (!isAtPos(ClimberPos.EXTEND)){
-                    changeState(ClimberPos.EXTEND);
+                if (isArmAtPos(ClimberArmPos.EXTEND)) break;
+                LeftMotor.set(TalonFXControlMode.PercentOutput, 0.1);
+                RightMotor.set(TalonFXControlMode.PercentOutput, 0.1);
+                if (LeftMotor.getStatorCurrent() > 3 || RightMotor.getStatorCurrent() > 3) {
+                    LeftMotor.set(TalonFXControlMode.PercentOutput, 0);
+                    RightMotor.set(TalonFXControlMode.PercentOutput, 0);
+                    changeState(ClimberState.DEFENSE);
+                    calibrated = true;
                 }
             break;
             case RETRACT:
-                if (!isAtPos(ClimberPos.RETRACT)){
-                    changeState(ClimberPos.RETRACT);
+                if (isArmAtPos(ClimberArmPos.RETRACT)) break;
+                LeftMotor.set(TalonFXControlMode.PercentOutput, -0.1);
+                RightMotor.set(TalonFXControlMode.PercentOutput, -0.1);
+                if (LeftMotor.getStatorCurrent() > 3 || RightMotor.getStatorCurrent() > 3) {
+                    LeftMotor.set(TalonFXControlMode.PercentOutput, 0);
+                    RightMotor.set(TalonFXControlMode.PercentOutput, 0);
+                    changeState(ClimberState.DEFENSE);
+                    calibrated = true;
                 }
             break;
-            case CALIBRATING:
-                LeftMotor.set(TalonFXControlMode.PercentOutput, -0.1);
-            break;
-        }   
+        }
+    }
+
+    private boolean isAtPos(ClimberState state) {
+        return getClimberStatus() == state;
     }
 
     @Override
@@ -71,9 +113,9 @@ public class Climber extends Subsystem {
     {
         switch(getClimberStatus())
         {
-            case DEFENSE:       changeState(ClimberState.EXTEND);   break;
-            case EXTEND:        changeState(ClimberState.RETRACT);  break;
-            case RETRACT:       changeState(ClimberState.EXTEND);   break;
+            case DEFENSE:       changeState(ClimberState.LEAN_FORWARD);   break;
+            case LEAN_FORWARD:        changeState(ClimberState.LEAN_BACKWARD);  break;
+            case LEAN_BACKWARD:       changeState(ClimberState.LEAN_FORWARD);   break;
             case CALIBRATING:   break;
         }
     }
