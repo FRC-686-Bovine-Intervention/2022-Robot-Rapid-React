085efbf9f085b6ceb778ddd7fc6023c36435fdbb
Michael Sims <michael@the-sims-family.net>
2022-02-20 17:25:22 -0500

    Imported ArmDebug code and arm now works with positions

--- Robot 2022 Rapid React/src/main/java/frc/robot/Subsystems/Subsystems/Intake.java
+++ Robot 2022 Rapid React/src/main/java/frc/robot/Subsystems/Subsystems/Intake.java
@@ -7,8 +7,9 @@ import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -23,20 +24,32 @@ public class Intake extends Subsystem {
     
     private TalonFX ArmMotor;
     private VictorSPX RollerMotor;
+    
+    private static final double kOuttakePercentOutput = -0.9;
+    private static final double kIntakePercentOutput = 0.7;
 
+    TrapezoidProfile.State calState = new TrapezoidProfile.State(ArmPosEnum.CALIBRATION.angleDeg, 0);
+    TrapezoidProfile.State scoringState = new TrapezoidProfile.State(ArmPosEnum.RAISED.angleDeg, 0);
+    TrapezoidProfile.State groundState = new TrapezoidProfile.State(ArmPosEnum.LOWERED.angleDeg, 0);
 
-    //Constants for controlling the arm. consider tuning these for your particular robot
-    final double armHoldUp = 0.08;
-    final double armHoldDown = 0.13;
-    final double armTravel = 0.5;
+    private static final double kCalibrationPercentOutput = 0.15;
 
-    final double armTimeUp = 0.5;
-    final double armTimeDown = 0.35;
+    private static final double kGroundHoldingThresholdDegrees = 2.0;
+    private static final double kGroundHoldingPercentOutput = -0.10;
 
-    //Varibles needed for the code
-    boolean burstMode = false;
-    double lastBurstTime = 0;
+    private static final double kGearRatio = 16.0 * 48.0/12.0;  // 16 in gearbox, 48t:12t sprockets
+    private static final double kEncoderUnitsPerRev = 2048 * kGearRatio;
+    private static final double kEncoderUnitsPerDeg = kEncoderUnitsPerRev/360.0;
 
+    private static final double kP = 0.08;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+    private static final double kMaxVelocityDegPerSecond = 120;
+    private static final double kMaxAccelerationDegPerSecSquared = 270;
+
+    private static final double kAtTargetThresholdDegrees = 1.0;
+
+    private ProfiledPIDController pid;
 
     private Intake()
     {
@@ -46,14 +59,18 @@ public class Intake extends Subsystem {
         ArmMotor.configFactoryDefault();
         ArmMotor.setInverted(TalonFXInvertType.CounterClockwise);
         ArmMotor.setNeutralMode(NeutralMode.Brake);
+
+        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(kMaxVelocityDegPerSecond, kMaxAccelerationDegPerSecSquared);
+        pid = new ProfiledPIDController(kP, kI, kD, constraints);
+        pid.reset(calState);
     
-        intakeStatus = IntakeState.DEFENSE;
         calibrated = false;
     }
     
     public enum ArmPosEnum {
         LOWERED(0),
-        RAISED(35236);
+        RAISED(105),
+        CALIBRATION(112);
 
         public final double angleDeg;
         ArmPosEnum(double angleDeg) {this.angleDeg = angleDeg;}
@@ -69,7 +86,7 @@ public class Intake extends Subsystem {
         CLIMBING,
         CALIBRATING
     }
-    public IntakeState intakeStatus;
+    public IntakeState intakeStatus = IntakeState.DEFENSE;
 
     @Override
     public void run()
@@ -79,38 +96,26 @@ public class Intake extends Subsystem {
         {
             case DEFENSE: default:
                 RollerMotor.set(VictorSPXControlMode.PercentOutput, 0);
-                if(targetPos != ArmPosEnum.RAISED)
-                {
-                    lastBurstTime = Timer.getFPGATimestamp();
-                    setTargetPos(ArmPosEnum.RAISED);
-                }
+                setTargetPos(ArmPosEnum.RAISED);
             break;
             case INTAKE:
-                if (isAtPos(ArmPosEnum.LOWERED)) {RollerMotor.set(VictorSPXControlMode.PercentOutput, 0.7);}
-                    if(targetPos != ArmPosEnum.LOWERED)
-                    {
-                        lastBurstTime = Timer.getFPGATimestamp();
-                        setTargetPos(ArmPosEnum.LOWERED);
-                    }
+                if(isAtPos(ArmPosEnum.LOWERED)) {RollerMotor.set(VictorSPXControlMode.PercentOutput, kIntakePercentOutput);}
+                else {setTargetPos(ArmPosEnum.LOWERED);}
             break;
             case OUTTAKE:
-                if (isAtPos(ArmPosEnum.RAISED)) {RollerMotor.set(VictorSPXControlMode.PercentOutput, -0.9);}
-                    if(targetPos != ArmPosEnum.RAISED)
-                    {
-                        lastBurstTime = Timer.getFPGATimestamp();
-                        setTargetPos(ArmPosEnum.RAISED);
-                    }
+                if(isAtPos(ArmPosEnum.RAISED)) {RollerMotor.set(VictorSPXControlMode.PercentOutput, kOuttakePercentOutput);}
+                else {setTargetPos(ArmPosEnum.RAISED);}
             break;
             case CLIMBING: break;
-            case CALIBRATING: calibrated = true; changeState(IntakeState.DEFENSE);/*
-                if (ArmMotor.getStatorCurrent() > 5)
+            case CALIBRATING:
+                if (checkFwdLimitSwitch())
                 {
                     ArmMotor.set(TalonFXControlMode.PercentOutput, 0);
                     changeState(IntakeState.DEFENSE);
                     calibrated = true;
                     break;
                 }
-                ArmMotor.set(TalonFXControlMode.PercentOutput, 0.2);*/
+                ArmMotor.set(TalonFXControlMode.PercentOutput, kCalibrationPercentOutput);
             break;
         }
         if (intakeStatus != IntakeState.CALIBRATING) setPos(targetPos);
@@ -119,7 +124,7 @@ public class Intake extends Subsystem {
     @Override
     public void runTestMode()
     {
-        intakeStatus = stateChooser.getSelected();
+        //intakeStatus = stateChooser.getSelected();
         if (stateChooser.getSelected() == IntakeState.CALIBRATING || calibrateEntry.getBoolean(false))
         {
             runCalibration();
@@ -135,35 +140,60 @@ public class Intake extends Subsystem {
         calibrated = false;
         changeState(IntakeState.CALIBRATING);
     }
+    @Override
+    public void disable() {
+        ArmMotor.set(TalonFXControlMode.PercentOutput, 0.0);
+        RollerMotor.set(VictorSPXControlMode.PercentOutput, 0);
+        calibrated = false;
+    }
 
     /**
      * @param pos is the desired pos
      * @param error is the radius of error
      * @return if the arm is within the radius of error from the desired pos
      */
-    public boolean isAtPos(ArmPosEnum pos, double error) {return ((currentPos >= pos.angleDeg - error)&&(currentPos <= pos.angleDeg + error));}
-    public boolean isAtPos(ArmPosEnum pos) {return true;/*return isAtPos(pos, 4);*/}
+    public boolean isAtPos(ArmPosEnum pos)
+    {
+        double currentAngleDegrees = encoderUnitsToDegrees(ArmMotor.getSelectedSensorPosition());
+        double targetDegrees = pos.angleDeg;
+
+        return (Math.abs(currentAngleDegrees - targetDegrees) < kAtTargetThresholdDegrees);
+    }
 
     public void setTargetPos(ArmPosEnum pos) {targetPos = pos;}
 
     private void setPos(ArmPosEnum pos)
     {
-        if(pos == ArmPosEnum.RAISED){
-            if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
-                ArmMotor.set(TalonFXControlMode.PercentOutput, armTravel);
-            }
-            else{
-                ArmMotor.set(TalonFXControlMode.PercentOutput, armHoldUp);
-            }
+        pid.setGoal(pos.angleDeg);
+        double pidOutput = 0.0;
+        double currentAngleDegrees = encoderUnitsToDegrees(ArmMotor.getSelectedSensorPosition());
+
+        pidOutput = pid.calculate(currentAngleDegrees);
+        ArmMotor.set(TalonFXControlMode.PercentOutput, pidOutput);
+
+        if ((pid.getGoal().position == ArmPosEnum.LOWERED.angleDeg) && (currentAngleDegrees < kGroundHoldingThresholdDegrees))
+        {
+            ArmMotor.set(TalonFXControlMode.PercentOutput, kGroundHoldingPercentOutput);
         }
-        else{
-            if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
-                ArmMotor.set(TalonFXControlMode.PercentOutput, -armTravel);
-            }
-            else{
-                ArmMotor.set(TalonFXControlMode.PercentOutput, -armHoldDown);
-            }
+    }
+
+    public static int degreesToEncoderUnits(double _degrees) {return (int)(_degrees * kEncoderUnitsPerDeg);}
+    public static double encoderUnitsToDegrees(double _encoderUnits) {return (double)(_encoderUnits / kEncoderUnitsPerDeg);}
+
+    public boolean checkFwdLimitSwitch()
+    {
+        boolean fwdLimitSwitchClosed = (ArmMotor.isFwdLimitSwitchClosed() == 1);
+        if (fwdLimitSwitchClosed)
+        {
+            // set the calibration position here
+            ArmMotor.setSelectedSensorPosition(degreesToEncoderUnits(ArmPosEnum.CALIBRATION.angleDeg));
+            pid.reset(calState);
+            pid.setGoal(calState);
+
+            // stop future calibration
+
         }
+        return fwdLimitSwitchClosed;
     }
 
     public void changeState(IntakeState newState)
