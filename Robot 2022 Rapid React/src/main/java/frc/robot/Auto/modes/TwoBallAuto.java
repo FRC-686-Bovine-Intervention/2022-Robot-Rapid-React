eb1c5b1427659d800ae26268949292528d019ae8
Bovine Intervention <rich.sims@bovineintervention.org>
2022-03-06 17:42:05 -0500

    wip

--- Robot 2022 Rapid React/src/main/java/frc/robot/Auto/modes/TwoBallAuto.java
+++ Robot 2022 Rapid React/src/main/java/frc/robot/Auto/modes/TwoBallAuto.java
@@ -10,11 +10,11 @@ import frc.robot.auto.actions.PathFollowerAction;
 import frc.robot.auto.actions.SetIntakeAction;
 import frc.robot.auto.actions.TurnToAngleAction;
 import frc.robot.auto.actions.WaitAction;
+import frc.robot.command_status.RobotState;
 import frc.robot.lib.util.Path;
 import frc.robot.lib.util.Path.Waypoint;
 import frc.robot.lib.util.PathSegment.Options;
 import frc.robot.lib.util.Vector2d;
-import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Intake.IntakeState;
 
 public class TwoBallAuto extends AutoMode{
@@ -29,47 +29,55 @@ public class TwoBallAuto extends AutoMode{
         boolean visionEnabled = false;
         Options driveOptions = new Options(maxSpeed, accel, lookaheadDist, visionEnabled);
 
+        double shotTime = 0.3;
+
         // Robot starts with front bumper aligned with tarmac tape, with bumper corner at apex corner of tarmax
         
         // path1: drive forward to collect one more ball
+        Vector2d ourBall4StartPos = FieldDimensions.ourBall4StartPose.getPosition();
         Vector2d ourBall4IntakePos = FieldDimensions.ourBall4.sub(Vector2d.magnitudeAngle(Constants.kCenterToFrontBumper, Units.degreesToRadians(-35.35))); 
         Path path1 = new Path();
-        path1.add(new Waypoint(FieldDimensions.ourBall4StartPose.getPosition(), driveOptions));    // start at tarmac tape
+        path1.add(new Waypoint(ourBall4StartPos, driveOptions));    // start at tarmac tape
         path1.add(new Waypoint(ourBall4IntakePos, driveOptions));   // stop when front bumpers reach center of ball position
 
         // path2: after turning around, drive from intake position to the left fender, passing through
         // a spot in front of the fender to get a good angle
         Vector2d lFenderApproachPos = FieldDimensions.lFenderCenter.sub(Vector2d.magnitudeAngle(48.0 ,FieldDimensions.lFenderApproachAngleRad));
+        Vector2d lFenderShotPos = FieldDimensions.lFenderShotPose.getPosition();
         Path path2 = new Path();
         path2.add(new Waypoint(ourBall4IntakePos, driveOptions));
         path2.add(new Waypoint(lFenderApproachPos, driveOptions));
-        path2.add(new Waypoint(FieldDimensions.lFenderShotPose.getPosition(), driveOptions));
+        path2.add(new Waypoint(lFenderShotPos, driveOptions));
 
         // path3: back up a short distance after shooting
-        Vector2d lFenderBackupPos = FieldDimensions.lFenderCenter.sub(Vector2d.magnitudeAngle(24.0 ,FieldDimensions.lFenderApproachAngleRad));
+        Vector2d lFenderBackupPos = lFenderShotPos.sub(Vector2d.magnitudeAngle(24.0 ,FieldDimensions.lFenderApproachAngleRad));
         Path path3 = new Path();
-        path3.add(new Waypoint(FieldDimensions.lFenderShotPose.getPosition(), driveOptions));
+        path3.add(new Waypoint(lFenderShotPos, driveOptions));
         path3.add(new Waypoint(lFenderBackupPos, driveOptions));
         path3.setReverseDirection();
 
-        // path4: after backing up and turning towards theirBall2, intake theirBall2, and shoot it into the hangar
-        Vector2d theirBall2IntakePos = FieldDimensions.theirBall2.sub(Vector2d.magnitudeAngle(Constants.kCenterToFrontBumper, Units.degreesToRadians(-35.35)));
-        Vector2d outtakePos = theirBall2IntakePos.add(new Vector2d(24.0, 0.0));
+        // path4: after backing up and turning towards theirBall3, intake theirBall3, and shoot it into the hangar
+        Vector2d theirBall3IntakePos = FieldDimensions.theirBall3.sub(Vector2d.magnitudeAngle(Constants.kCenterToFrontBumper, Units.degreesToRadians(-35.35)));
+        Vector2d outtakePos = theirBall3IntakePos.add(new Vector2d(24.0, 0.0));
         Path path4 = new Path();
         path4.add(new Waypoint(lFenderBackupPos, driveOptions));
-        path4.add(new Waypoint(theirBall2IntakePos, driveOptions));
+        path4.add(new Waypoint(theirBall3IntakePos, driveOptions));
         path4.add(new Waypoint(outtakePos, driveOptions));
 
         // path5: after turning around, get close to ourBall5 on the other side of the field
+        Vector2d ourBall5EndPos = FieldDimensions.theirBall3.add(new Vector2d(-20, -12));
         Path path5 = new Path();
         path5.add(new Waypoint(outtakePos, driveOptions));
-        path5.add(new Waypoint(FieldDimensions.ourBall5EndPose.getPosition(), driveOptions));
+        path5.add(new Waypoint(ourBall5EndPos, driveOptions));
 
 
         //================================================================
         // TWO BALL AUTO
         // (plus yeeting other teams ball into hangar)
         //================================================================
+        // set initial pose
+        RobotState.getInstance().reset(FieldDimensions.ourBall4StartPose);
+
         runAction(new WaitAction(0.0));     // TODO: use programmable delay from Shuffleboard
 
         // drive forward, intake ball
@@ -79,18 +87,20 @@ public class TwoBallAuto extends AutoMode{
         runAction(new ParallelAction(Arrays.asList(new SetIntakeAction(IntakeState.DEFENSE), new TurnToAngleAction(180 - 35.25))));
         runAction(new PathFollowerAction(path2));
         runAction(new SetIntakeAction(IntakeState.OUTTAKE));
-        runAction(new WaitAction(0.3));
+        runAction(new WaitAction(shotTime));
 
-        // back up a short distance, turn towards theirBall2
+        // back up a short distance, turn towards theirBall3
         runAction(new ParallelAction(Arrays.asList(new SetIntakeAction(IntakeState.DEFENSE), new PathFollowerAction(path3))));
         runAction(new TurnToAngleAction(-54.75));
-        // drive to theirBall2, intake, drive a little longer then outtake into hangar
+        // drive to theirBall3, intake, drive a little longer then outtake into hangar
         runAction(new ParallelAction(Arrays.asList(new SetIntakeAction(IntakeState.INTAKE), new PathFollowerAction(path4))));
         runAction(new SetIntakeAction(IntakeState.OUTTAKE_GROUND));
-        runAction(new WaitAction(0.3));
+        runAction(new WaitAction(shotTime));
+
+        // TODO?  Could hide ball behind truss structure if we have enough time
 
         // turn around, head towards ourBall5
-        runAction(new TurnToAngleAction(180));
-        runAction(new PathFollowerAction(path5));
+        runAction(new TurnToAngleAction(180.0));
+        runAction(new ParallelAction(Arrays.asList(new SetIntakeAction(IntakeState.INTAKE), new PathFollowerAction(path5))));
     }
 }
