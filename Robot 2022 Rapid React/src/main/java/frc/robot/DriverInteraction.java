d554e08a12a9bff9585bcdac0e78907864c81e15
Michael Sims <michael@the-sims-family.net>
2022-03-07 19:47:39 -0500

    started calibration

--- Robot 2022 Rapid React/src/main/java/frc/robot/DriverInteraction.java
+++ Robot 2022 Rapid React/src/main/java/frc/robot/DriverInteraction.java
@@ -36,9 +36,9 @@ public class DriverInteraction {
 
     public void run()
     {
-        driving = !controls.getButton(Controls.ButtonControlEnum.CLIMBERNEXTSTAGE);
-        if (driving) Drive.getInstance().setOpenLoop(controls.getDriveCommand());
-        else Climber.getInstance().setTargetPos(controls.getAxis(JoystickEnum.THRUSTMASTER).y*+1.0);
+        // driving = !controls.getButton(Controls.ButtonControlEnum.CLIMBERNEXTSTAGE);
+        // if (driving) Drive.getInstance().setOpenLoop(controls.getDriveCommand());
+        // else Climber.getInstance().setTargetPos(controls.getAxis(JoystickEnum.THRUSTMASTER).y*+1.0);
         if (controls.getButton(Controls.ButtonControlEnum.INTAKE))
         {
             drive.setOpenLoop(controls.getDriveCommand());
