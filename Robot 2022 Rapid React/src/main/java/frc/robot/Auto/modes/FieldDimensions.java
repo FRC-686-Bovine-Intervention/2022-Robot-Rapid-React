fe4066e91af58aed5d37b767da5f507adfb292c9
Michael Sims <michael@the-sims-family.net>
2022-08-05 09:42:56 -0400

    Updated FieldDimensions

--- Robot 2022 Rapid React/src/main/java/frc/robot/Auto/modes/FieldDimensions.java
+++ Robot 2022 Rapid React/src/main/java/frc/robot/Auto/modes/FieldDimensions.java
@@ -39,11 +39,11 @@ public class FieldDimensions
                                     twoBallAutoInitialHeadingRad);
     
     // Ball Positions
-    public static final Vector2d redBall2of2  = new Vector2d(115.5, -31.5);   // near hangar
-    public static final Vector2d blueBall2of2  = new Vector2d(116, -29);   // near hangar
+    public static final Vector2d redBall2of2  = new Vector2d(116, -28.5);   // near hangar
+    public static final Vector2d blueBall2of2  = new Vector2d(117, -26);   // near hangar
     public static Vector2d[] ball2of2  = new Vector2d[]{redBall2of2, blueBall2of2};
-    public static final Vector2d redTheirBall = new Vector2d(91, -85);    // left wall
-    public static final Vector2d blueTheirBall = new Vector2d(93.5, -84);    // left wall
+    public static final Vector2d redTheirBall = new Vector2d(95, -85);    // left wall
+    public static final Vector2d blueTheirBall = new Vector2d(97, -82);    // left wall
     public static Vector2d[] theirBall  = new Vector2d[]{redTheirBall, blueTheirBall};//DriverStation.getAlliance() == Alliance.Red ? redBall2of2 : blueBall2of2;   // near hangar
     public static final Vector2d twoBallAutoFinalTarget  = new Vector2d(51, -127);   // other side of field, left wall
     
@@ -58,11 +58,11 @@ public class FieldDimensions
     public static final Pose threeBallAutoStartingPose = new Pose(fenderShotPos, Units.degreesToRadians(180));
     
     // Ball Positions
-    public static final Vector2d redBall2of3   = new Vector2d(116, 31);   // right wall
-    public static final Vector2d blueBall2of3   = new Vector2d(116, 28.5);   // right wall
+    public static final Vector2d redBall2of3   = new Vector2d(115, 31);   // right wall
+    public static final Vector2d blueBall2of3   = new Vector2d(114, 31);   // right wall
     public static final Vector2d[] ball2of3 = new Vector2d[]{redBall2of3,blueBall2of3};
-    public static final Vector2d redBall3of3   = new Vector2d(97, -83);   // near center
-    public static final Vector2d blueBall3of3   = new Vector2d(92, -86);   // near center
+    public static final Vector2d redBall3of3   = new Vector2d(95, -83);   // near center
+    public static final Vector2d blueBall3of3   = new Vector2d(93, -85);   // near center
     public static final Vector2d[] ball3of3 = new Vector2d[]{redBall3of3,blueBall3of3};
     public static final Vector2d threeBallAutoFinalTarget  = new Vector2d(51, 127);   // other side of field, left wall
     
